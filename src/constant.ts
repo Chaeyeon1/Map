1639a508f650914d86666b0c0f458c82ea573135
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2024-01-31 17:33:39 +0900

    테스트

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,5 @@
-// export const DEFAULT_URL = 'http://localhost:3000';
-export const DEFAULT_URL = 'http://15.164.221.35:3000';
+export const DEFAULT_URL = 'http://localhost:3000';
+// export const DEFAULT_URL = 'http://15.164.221.35:3000';
 // export const DEFAULT_URL = 'http://localhost:3001';
 export const COIN = ['wap', 'mus', 'app', 'pknu', 'pus', 'pufs'];
 export const COIN_URL = [
