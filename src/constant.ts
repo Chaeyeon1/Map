f82d0239aa78205102294b6e5969b921ef4e395e
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2024-02-02 17:33:04 +0900

    default url 변경

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,5 @@
-export const DEFAULT_URL = 'http://localhost:3000';
-// export const DEFAULT_URL = 'http://15.164.221.35:3000';
+// export const DEFAULT_URL = 'http://localhost:3000';
+export const DEFAULT_URL = 'http://15.164.221.35:3000';
 export const COIN = ['wap', 'app', 'mus', 'pknu', 'pus', 'bufs'];
 export const COIN_URL = [
   '/wap.png',
